1b1fa51b1ff7e52905f706f88b8610af566ed2c7
krishnasingh281 <samar.phone.home@gmail.com>
2025-02-09 19:41:14 +0530

    auth done

--- alumini_project/alumini_project/settings.py
+++ alumini_project/alumini_project/settings.py
@@ -145,3 +145,8 @@ REST_FRAMEWORK = {
 # ]
 
 # CORS_ALLOW_ALL_ORIGINS = True
+
+
+INSTALLED_APPS += ['corsheaders']
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+CORS_ALLOWED_ORIGINS = ["https://your-frontend-url.com"]
